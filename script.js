ef76e329abe16257f10e25fad952622056bc1292
PranayTyagi1 <>
2021-04-30 14:45:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.calculate').addEventListener('click', function () {
+  let number = document.querySelector('.input').value
+  let answer = number * number
+  alert(answer)
+})
